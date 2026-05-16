03cc726a01ea77f5f4cae1bdc68c3d1d0c5ced00
rezkaVkei33 <162965009+rezkaVkei33@users.noreply.github.com>
2026-05-16 22:02:36 +0700

    btn tambah

--- application/views/adminweb/tentang/arsip/tambah_arsip.php
+++ application/views/adminweb/tentang/arsip/tambah_arsip.php
@@ -45,8 +45,8 @@
                 </form>
             </div>
             <div class="modal-footer border-0 pt-0 pb-4">
-                <button type="button" class="btn btn-outline-poltek" data-bs-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-poltek" form="dataForm">Simpan Data</button>
+                <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-primary bg-gradient-info" form="dataForm">Simpan Data</button>
             </div>
         </div>
     </div>
