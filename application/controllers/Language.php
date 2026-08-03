6d7a8a1266a6b9d8169f5e170e59df00fb2e4cbc
rezkaVkei33 <162965009+rezkaVkei33@users.noreply.github.com>
2026-08-03 14:16:59 +0700

    language

--- application/controllers/Language.php
+++ application/controllers/Language.php
@@ -15,27 +15,32 @@ class Language extends MY_Controller
     /**
      * Ganti bahasa
      */
+    // public function change($language = 'indonesia')
+    // {
+
+    //     $language = strtolower($language);
+
+    //     if (!in_array($language, $this->available)) {
+
+    //         $language = 'indonesia';
+
+    //     }
+
+    //     $this->session->set_userdata('site_language', $language);
+
+    //     if ($this->input->server('HTTP_REFERER')) {
+
+    //         redirect($this->input->server('HTTP_REFERER'));
+
+    //     }
+
+    //     redirect(base_url());
+
+    // }
     public function change($language = 'indonesia')
-    {
-
-        $language = strtolower($language);
-
-        if (!in_array($language, $this->available)) {
-
-            $language = 'indonesia';
-
-        }
-
-        $this->session->set_userdata('site_language', $language);
-
-        if ($this->input->server('HTTP_REFERER')) {
-
-            redirect($this->input->server('HTTP_REFERER'));
-
-        }
-
-        redirect(base_url());
-
-    }
+{
+    echo "METHOD CHANGE DIPANGGIL";
+    die;
+}
 
 }
