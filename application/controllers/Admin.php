9e4378a4861112eb1d9c0bc7c7da997f6ebc1383
rezkaVkei33 <162965009+rezkaVkei33@users.noreply.github.com>
2026-08-03 13:47:10 +0700

    MY_

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends Admin_Controller {
+class Admin extends MY_Admin_controller {
 
     public function __construct() {
         parent::__construct();
--- application/core/MY_Admin_controller.php
+++ application/core/MY_Admin_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin_Controller extends MY_Controller
+class MY_Admin_controller extends MY_Controller
 {
 
     public function __construct()
