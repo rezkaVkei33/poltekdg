b02aac4e997f9d062420802eb668e266b52e3ff9
rezkaVkei33 <162965009+rezkaVkei33@users.noreply.github.com>
2026-08-03 18:49:04 +0700

    Admin_Controller

--- application/core/MY_Admin_Controller.php
+++ application/core/MY_Admin_Controller.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once(APPPATH . 'core/MY_Admin_Controller.php');
 
 class MY_Admin_Controller extends MY_Controller
 {
